cf4d5772f8313c6fd93af149020842c5050c7333
drevil6666 <33528184+drevil6666@users.noreply.github.com>
2017-11-09 23:27:45 +0300

    Update Currency.cpp

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -99,7 +99,7 @@ bool Currency::generateGenesisBlock() {
 bool Currency::getBlockReward(size_t medianSize, size_t currentBlockSize, uint64_t alreadyGeneratedCoins,
   uint64_t fee, uint64_t& reward, int64_t& emissionChange) const {
   assert(alreadyGeneratedCoins <= m_moneySupply);
-  assert(m_emissionSpeedFactor > 0 && m_emissionSpeedFactor <= 8 * sizeof(uint64_t));
+  assert(m_emissionSpeedFactor > 0 && m_emissionSpeedFactor <= 16 * sizeof(uint64_t));
 
   uint64_t baseReward = (m_moneySupply - alreadyGeneratedCoins) >> m_emissionSpeedFactor;
 
